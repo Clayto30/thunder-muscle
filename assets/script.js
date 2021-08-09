6edc209142535617f422b085126e319a72260c0b
Clayton Goff <cltngoff@gmail.com>
2021-08-08 21:54:46 -0700

    name/values display to text div

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,23 @@ hogman = {
     hp: 115
 }
 
-const displayChar = function() {
-    $("#textDiv").empty().append("You chose a character!");
+const displayChar = function () {
+    //$("#textDiv").empty().append("You chose a character!");
 };
 
 $("#charBtn").on("click", function () {
     event.preventDefault();
+    function displayRadioValue() {
+        var ele = document.getElementsByName('char');
+
+        for (i = 0; i < ele.length; i++) {
+            if (ele[i].checked)
+                document.getElementById("textDiv").innerHTML
+                    = "You chose " + ele[i].value;
+        }
+    }
+    displayRadioValue();
     $("#choiceDiv").empty();
     console.log(this.value)
-    displayChar();
+    //displayChar();
 });
