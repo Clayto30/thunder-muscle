55f7bc070906809821cb4d047e51ef1335275460
Clayton Goff <cltngoff@gmail.com>
2021-08-27 15:17:19 -0700

    added charcter objects

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,37 @@
-shrimpy = {
+const shrimpy = {
     name: "Shrimpy McDee",
-    hp: 100
+    hp: 100,
+    charImg: "/shrimpy.jpg"
 }
 
-hogman = {
+const hogman = {
     name: "Hogman Wilder",
-    hp: 115
+    hp: 115,
+    charImg: ""
+}
+
+const roger = {
+    name: "Roger 'The Hammer' Stein",
+    hp: 100,
+    charImg: "/assets/hammer.jpg"
+}
+
+const bobby = {
+    name: "Young Bobby",
+    hp: 100,
+    charImg: "/assets/young.jpg"
+}
+
+const swole = {
+    name: "SwoleFather",
+    hp: 115,
+    charImg: "/assets/swole.jpg"
+}
+
+var char = {
+    name: "",
+    hp: 100,
+    charImg: ""
 }
 
 $("#charBtn").on("click", function () {
@@ -17,23 +43,40 @@ $("#charBtn").on("click", function () {
             if (ele[i].checked)
                 document.getElementById("textDiv").innerHTML
                     = "You chose " + ele[i].value;
+            if (ele[i].value === "Shrimpy McDee") {
+                console.log(char);
+            } else if (ele[i].value === "Hogman Wilder") {
+                return char = hogman
+            } else if (ele[i].value === "Young Bobby") {
+                return char = bobby
+            } else if (ele[i].value === "Roger the Hammer Stein") {
+                return char = roger
+            } else if (ele[i].value === "Swole Father") {
+                return char = swole
+            } else {
+                console.log("It didn't work")
+                break;
+            }
         }
     }
     displayRadioValue();
+    console.log(char.charImg + "2");
     $("#choiceDiv").empty();
-    const text1 = function() {
-        document.getElementById("textDiv").innerHTML = "Your character pins the opponent!"
+    document.getElementById("imgDiv").innerHTML = "<img src=" + shrimpy.charImg + ">"
+    console.log(char.name + "3");
+    const text1 = function () {
+        document.getElementById("textDiv").innerHTML = char.name + " pins the opponent!"
     };
-    const text2 = function() {
+    const text2 = function () {
         document.getElementById("textDiv").innerHTML = "<h3>3!</h3>"
     };
-    const text3 = function() {
+    const text3 = function () {
         document.getElementById("textDiv").innerHTML = "<h2>2!</h2>"
     };
-    const text4 = function() {
+    const text4 = function () {
         document.getElementById("textDiv").innerHTML = "<h1>1!</h1>"
     };
-    const text5 = function() {
+    const text5 = function () {
         document.getElementById("textDiv").innerHTML = "Your character wins the match! The crowd goes wild!"
     };
     setTimeout(text1, 3000);
