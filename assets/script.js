b0874ef25960a3a8dcacec5e0ef40e7a59440829
Clayton Goff <cltngoff@gmail.com>
2021-09-14 11:13:31 -0400

    images display

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,31 @@
 const shrimpy = {
     name: "Shrimpy McDee",
     hp: 100,
-    charImg: "/shrimpy.jpg"
+    charImg: "assets/shrimpy.jpg"
 }
 
 const hogman = {
     name: "Hogman Wilder",
     hp: 115,
-    charImg: ""
+    charImg: "assets/hammer.jpg"
 }
 
 const roger = {
     name: "Roger 'The Hammer' Stein",
     hp: 100,
-    charImg: "/assets/hammer.jpg"
+    charImg: "assets/hammer.jpg"
 }
 
 const bobby = {
     name: "Young Bobby",
     hp: 100,
-    charImg: "/assets/young.jpg"
+    charImg: "assets/young.jpg"
 }
 
 const swole = {
     name: "SwoleFather",
     hp: 115,
-    charImg: "/assets/swole.jpg"
+    charImg: "assets/swole.jpg"
 }
 
 var char = {
@@ -40,11 +40,11 @@ $("#charBtn").on("click", function () {
         var ele = document.getElementsByName('char');
 
         for (i = 0; i < ele.length; i++) {
-            if (ele[i].checked)
+            if (ele[i].checked) {
                 document.getElementById("textDiv").innerHTML
                     = "You chose " + ele[i].value;
             if (ele[i].value === "Shrimpy McDee") {
-                console.log(char);
+                return char = shrimpy;
             } else if (ele[i].value === "Hogman Wilder") {
                 return char = hogman
             } else if (ele[i].value === "Young Bobby") {
@@ -58,11 +58,12 @@ $("#charBtn").on("click", function () {
                 break;
             }
         }
+        }
     }
     displayRadioValue();
     console.log(char.charImg + "2");
     $("#choiceDiv").empty();
-    document.getElementById("imgDiv").innerHTML = "<img src=" + shrimpy.charImg + ">"
+    document.getElementById("imgDiv").innerHTML = "<img src=" + char.charImg + ">"
     console.log(char.name + "3");
     const text1 = function () {
         document.getElementById("textDiv").innerHTML = char.name + " pins the opponent!"
@@ -77,7 +78,7 @@ $("#charBtn").on("click", function () {
         document.getElementById("textDiv").innerHTML = "<h1>1!</h1>"
     };
     const text5 = function () {
-        document.getElementById("textDiv").innerHTML = "Your character wins the match! The crowd goes wild!"
+        document.getElementById("textDiv").innerHTML = char.name + " wins the match! The crowd goes wild!"
     };
     setTimeout(text1, 3000);
     setTimeout(text2, 6000);
